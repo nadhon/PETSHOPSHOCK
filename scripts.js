071e50444aaca0e000a8ac0f2a77c0a0c00c332e
njsa <paladinoderua@gmail.com>
2024-08-21 11:14:09 -0300

    remoção de uma linha de código

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 import {createClient } from '@supabase/supabase-js'
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
-const {data: todos, error} = await supabase.from('todos').selects('*')
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('Mensagem enviada com sucesso!');
